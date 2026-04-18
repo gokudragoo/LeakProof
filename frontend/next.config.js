d070b06f818b79bba97a964e42400dcf3e41a0e1
gokudragoo <nikku.cortex@gmail.com>
2026-04-19 01:48:28 +0530

    updating

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['@cofhe/sdk'],
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
 };
 
 module.exports = nextConfig;
